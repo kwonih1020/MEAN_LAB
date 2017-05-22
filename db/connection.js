73b6117432be2add34f67c5aacb0228b7b24c787
Jeong Hoon Koo <junghoone17@gmail.com>
2017-05-22 14:51:09 -0400

    Full CRUD on Question model & working on CRUD for Answer model

--- db/connection.js
+++ db/connection.js
@@ -1,23 +1,23 @@
 let mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost/questions');
+mongoose.connect('mongodb://localhost/meanlab');
 
 let Schema = mongoose.Schema;
+
+let AnswerSchema = new Schema({
+  content: String,
+  author: String
+})
 
 let QuestionSchema = new Schema({
   title: String,
   content: String,
-  author: String
-  // answers: [AnswerSchema]
+  author: String,
+  answers: [AnswerSchema]
 })
 
-// let AnswerSchema = new Answer({
-//   content: String,
-//   author: String
-// })
-
 let Question = mongoose.model("Question", QuestionSchema);
-// let Answer = mongoose.model("Answer", QuestionSchema);
+let Answer = mongoose.model("Answer", QuestionSchema);
 
 module.exports = {
-  Question
+  Question, Answer
 }
--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,10 @@
 let mongoose = require("./connection.js");
 let seedData = require("./seeds");
 let Question = mongoose.Question;
+let Answer = mongoose.Answer;
 
 
-// let Question = mongoose.model("Question");
-// let Answer = mongoose.model("Answer");
+
 
 Question.remove({}).then(function(){
   Question.collection.insert(seedData).then(function(){
